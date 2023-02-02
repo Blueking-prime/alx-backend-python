62fd4bc07df13d274ea8af8975e8d9bb28eeaaf4
Nnamdi Anyanwu <blueking189@gmail.com>
2023-02-02 13:12:04 +0100

    Revert changes

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 '''a typed tuple creation function'''
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Sequence, Tuple
 
 
-def element_length(lst: Iterable) -> List:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     return [(i, len(i)) for i in lst]
