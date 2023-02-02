d2fef774550dcb97c5837eb70c3d3d578494f70f
Nnamdi Anyanwu <blueking189@gmail.com>
2023-02-02 13:11:28 +0100

    Test duck typing

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''a typed tuple creation function'''
+from typing import Iterable, List, Tuple
 
 
-def element_length(lst):
+def element_length(lst: Iterable) -> List:
     return [(i, len(i)) for i in lst]
